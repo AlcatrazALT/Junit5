38d33c0bf24862c2163ff46ef7064242474f9a12
AlcatrazALT <olegmazalevsky21@yandex.ru>
2020-04-29 13:02:17 +0700

    Before After Annotation

--- src/test/java/com/juit5/MathUtilsTest.java
+++ src/test/java/com/juit5/MathUtilsTest.java
@@ -1,22 +1,52 @@
 package com.juit5;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MathUtilsTest {
 
+    MathUtils mathUtils;
+
+    @BeforeEach
+    void init() {
+        mathUtils = new MathUtils();
+    }
+
+    @BeforeAll
+    static void beforeAll(){
+        System.out.println("BEFORE ALL -> ");
+    }
+
+    @BeforeEach
+    void beforeEach(){
+        System.out.println("BEFORE EACH -> ");
+    }
+
+    @AfterEach
+    void afterEach(){
+        System.out.println("AFTER EACH -> ");
+    }
+
+    @AfterAll
+    static void afterAll(){
+        System.out.println("AFTER ALL -> ");
+    }
+
     @Test
-    void test(){
-        MathUtils mathUtils = new MathUtils();
+    void test() {
         int expected = 2;
         int actual = mathUtils.add(1, 1);
         assertEquals(expected, actual, "method should add 2 numbers");
     }
 
     @Test
-    void testComputeCircleArea(){
-        MathUtils mathUtils = new MathUtils();
+    void testDivide() {
+        assertThrows(ArithmeticException.class, () -> mathUtils.divide(1, 0), "Divide by zero should throw exception");
+    }
+
+    @Test
+    void testComputeCircleArea() {
         assertEquals(314.1592653589793, mathUtils.computeCircleArea(10), "should return right circle area");
     }
 }
