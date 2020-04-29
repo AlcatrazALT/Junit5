45642855ebd982e0a9637333c6156229c0b7d216
AlcatrazALT <olegmazalevsky21@yandex.ru>
2020-04-29 11:35:45 +0700

    Run simple test

---
+++ src/main/java/com/juit5/MathUtils.java
@@ -0,0 +1,7 @@
+package com.juit5;
+
+public class MathUtils {
+    public int add(int a, int b){
+        return a + b;
+    }
+}
---
+++ src/test/java/com/juit5/MathUtilsTest.java
@@ -0,0 +1,13 @@
+package com.juit5;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MathUtilsTest {
+
+    @Test
+    void test(){
+        System.out.println("this is run test");
+    }
+}
